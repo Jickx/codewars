7390e15271c5ef9700ee93f1de3c1853055ee267
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-01 15:29:32 +0300

    add simple_multiplication

---
+++ simple_multiplication.py
@@ -0,0 +1,10 @@
+# Multiplying a given number by eight if it is an even number
+# and by nine otherwise.
+
+def simple_multiplication(number):
+    return number * 8 if number % 2 == 0 else number * 9
+
+assert simple_multiplication(1) == 9
+assert simple_multiplication(8) == 64
+assert simple_multiplication(4) == 32
+assert simple_multiplication(5) == 45
