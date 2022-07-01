9717d2f4d5ae16f9214e8959ad71f91033b92412
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-01 09:32:11 +0300

    listfilter refactor

--- listfilter.py
+++ listfilter.py
@@ -1,10 +1,9 @@
-# return a new list with the strings filtered out
-def filter_list(l):
-    list2 = []
-    for i in l:
-        if isinstance(i, str) == True:
-            continue
-        else:
-            list2.append(i)
+# Create a function that takes a list of non-negative integers and strings and
+# returns a new list with the strings filtered out.
 
-    return (list2)
+def filter_list(l):
+    return [i for i in l if not isinstance(i, str)]
+
+assert filter_list([1,2,'a','b']) == [1,2]
+assert filter_list([1,'a','b',0,15]) == [1,0,15]
+assert filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
