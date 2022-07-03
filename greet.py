29357a680fe6d90b2da1b690f97253eb9f5db27b
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-03 16:48:50 +0300

    greet.py

---
+++ greet.py
@@ -0,0 +1,9 @@
+# Make a function that will return a greeting statement that uses an input;
+# your program should return, "Hello, <name> how are you doing today?".
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+assert greet('Ryan') == "Hello, Ryan how are you doing today?"
+assert greet('Shingles') == "Hello, Shingles how are you doing today?"
