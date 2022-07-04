394333a616ae61190d68e4ec4bc9ff68710cd636
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-04 19:25:55 +0300

    add reverse_words.py

---
+++ 7_kyu/reverse_words.py
@@ -0,0 +1,20 @@
+# Complete the function that accepts a string parameter,
+# and reverses each word in the string. All spaces in 
+# the string should be retained.
+
+def reverse_words(text):
+    result = ''
+    word = ''
+    for i in text:
+        if i == ' ':
+            result += word[::-1]
+            word = ''
+            result += i
+        else:
+            word += i
+    return result + word[::-1]
+
+# print(reverse_words('This is an example!'))
+
+assert reverse_words("This is an example!") == "sihT si na !elpmaxe"
+assert reverse_words("double  spaces") == "elbuod  secaps"
