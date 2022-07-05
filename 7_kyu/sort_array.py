748c4896f7df85377842426590f311f5103e70a2
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-05 12:16:18 +0300

    refactor sort_array.py

--- 7_kyu/sort_array.py
+++ 7_kyu/sort_array.py
@@ -3,20 +3,8 @@
 # their original positions.
 
 def sort_array(lst):
-    result = [None] * len(lst)
-    odds = []
-    for i in range(len(lst)):
-        if lst[i] % 2 == 0:
-            result[i] = lst[i]   
-        else:
-            odds.append(lst[i])
-    sorted_odds = sorted(odds)
-    sorted_odds.reverse()
-    for i in range(len(result)):
-        if result[i] == None:
-            result[i] = sorted_odds.pop()
-    return result
-
+    odds = sorted((i for i in lst if i % 2 != 0), reverse = True)
+    return [i if i % 2 == 0 else odds.pop() for i in lst]
 
 assert sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
 assert sort_array([5, 3, 1, 8, 0]) == [1, 3, 5, 8, 0]
