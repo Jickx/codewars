e3cb1039c4dadae6febacda9ddcd0186cd34f41c
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-19 13:49:31 +0300

    refactor

--- 5_kyu/move_zeros.py
+++ 5_kyu/move_zeros.py
@@ -2,23 +2,14 @@
 # end, preserving the order of the other elements.
 
 def move_zeros(lst: list[int]) -> list[int]:
-    result = []
-    i = 0
-    ctr = 0
-    while i < len(lst):
-        if lst[i] == 0:
-            ctr += 1
-            i += 1
-            continue
-        else:
-            result.append(lst[i])
-            i += 1
-    result.extend([0 for i in range(ctr)])
-    return result
+    result = [i for i in lst if i != 0]
+    return result + [0] * (len(lst) - len(result))
 
 
-assert move_zeros([1, 0, 1, 2, 0, 1, 3]) == [1, 1, 2, 1, 3, 0, 0]
-assert move_zeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]) == [
+assert move_zeros([
+    1, 0, 1, 2, 0, 1, 3]) == [1, 1, 2, 1, 3, 0, 0]
+assert move_zeros([
+    1, 2, 0, 1, 0, 1, 0, 3, 0, 1]) == [
     1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
 assert move_zeros([
     9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9
