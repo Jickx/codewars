ee998ce644e957e9146263aa8066298c2eb258e9
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-19 12:47:43 +0300

    format

--- 6_kyu/order.py
+++ 6_kyu/order.py
@@ -1,8 +1,8 @@
-# Your task is to sort a given string. Each word in the string will contain a
-# single number. This number is the position the word should have in the result.
-# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
-# If the input string is empty, return an empty string. The words in the input
-# String will only contain valid consecutive numbers.
+# Your task is to sort a given string. Each word in the string will contain
+# a single number. This number is the position the word should have in the
+# result. Note: Numbers can be from 1 to 9. So 1 will be the first word
+# (not 0). If the input string is empty, return an empty string. The words
+# in the input. String will only contain valid consecutive numbers.
 
 
 def order(sentence):
@@ -12,9 +12,11 @@ def order(sentence):
         for i in word:
             if i.isnumeric():
                 result[int(i) - 1] = word
-                break   
+                break
     return " ".join(result)
 
-assert order("is2 Thi1s T4est 3a")  ==  "Thi1s is2 3a T4est"
-assert order("4of Fo1r pe6ople g3ood th5e the2") == "Fo1r the2 g3ood 4of th5e pe6ople"
-assert order("") ==  ""
+
+assert order("is2 Thi1s T4est 3a") == "Thi1s is2 3a T4est"
+assert order(
+    "4of Fo1r pe6ople g3ood th5e the2") == "Fo1r the2 g3ood 4of th5e pe6ople"
+assert order("") == ""
